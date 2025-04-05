3a4cfc81b33b93d061200befc34ef2226f54e0ba
taintedelephant <36199619+taintedelephant@users.noreply.github.com>
2025-04-05 17:28:05 +0100

    hero box opacity

--- client/src/components/home/Hero.tsx
+++ client/src/components/home/Hero.tsx
@@ -3,7 +3,7 @@ import { Link } from "wouter";
 const Hero = () => {
   return (
     <section className="relative">
-      <div className="absolute inset-0 bg-gray-900 bg-opacity-50"></div>
+      <div className="absolute inset-0 bg-gray-900 bg-opacity-20"></div>
       <div
         className="h-96 md:h-[500px] w-full bg-cover bg-center"
         style={{
@@ -12,7 +12,7 @@ const Hero = () => {
       ></div>
       <div className="absolute inset-0 flex items-center">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 w-full">
-          <div className="max-w-lg bg-white bg-opacity-85 p-6 md:p-8 rounded-lg shadow-lg">
+          <div className="max-w-lg bg-white bg-opacity-35 p-6 md:p-8 rounded-lg shadow-lg">
             <h1 className="text-3xl md:text-4xl font-heading font-bold mb-2">
               Start Your Driving Journey Today
             </h1>
@@ -28,7 +28,7 @@ const Hero = () => {
               </Link>
               <a
                 href="#services"
-                className="bg-white border border-primary text-primary px-6 py-2 rounded font-medium text-center hover:bg-primary hover:text-white transition inline-block"
+                className="bg-white border bg-opacity-70 border-primary text-primary px-6 py-2 rounded font-medium text-center hover:bg-primary hover:text-white transition inline-block"
               >
                 My Services
               </a>
