7e5f139590d262f90d267054563e71675e0f121f
taintedelephant <36199619+taintedelephant@users.noreply.github.com>
2025-04-05 17:43:55 +0100

    box in hero section changes

--- client/src/components/home/Hero.tsx
+++ client/src/components/home/Hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <section className="relative">
       <div className="absolute inset-0 bg-gray-900 bg-opacity-20"></div>
       <div
-        className="h-96 md:h-[500px] w-full bg-cover bg-center"
+        className="h-96 md:h-[600px] w-full bg-cover bg-center"
         style={{
           backgroundImage: "url('/mercedes-c-class-mirrored.png')",
         }}
@@ -13,13 +13,10 @@ const Hero = () => {
       <div className="absolute inset-0 flex items-center">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 w-full">
           <div className="max-w-lg bg-white bg-opacity-35 p-6 md:p-8 rounded-lg shadow-lg">
-            <h1 className="text-3xl md:text-4xl font-heading font-bold mb-2">
+            <h1 className="text-3xl md:text-4xl font-heading font-bold mb-6 text-primary">
               Start Your Driving Journey Today
             </h1>
-            <p className="text-gray-600 mb-6">
-              Professional, patient instruction to help you become a test-ready
-              driver.
-            </p>
+           
             <div className="flex flex-row gap-4">
               <Link href="/booking">
                 <div className="bg-primary text-white px-6 py-2 rounded font-medium text-center hover:bg-opacity-90 transition inline-block cursor-pointer">
